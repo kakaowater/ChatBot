2e632fb885d2016f1090f6c08dad8a17d35ad3a5
kakaowater <105254212+kakaowater@users.noreply.github.com>
2022-06-14 12:42:11 +0900

    Update inference.py

--- codes/inference.py
+++ codes/inference.py
@@ -1,66 +1,75 @@
 # -*- coding: utf-8 -*-
 
-############################### TODO ##########################################
-# 필요한 모듈 불러오기
-###############################################################################
-
-import argparse
 import os
 import pickle
+import argparse
+
+from to_array.bert_to_array import BERTToArray
+from models.bert_slot_model import BertSlotModel
+
 import tensorflow as tf
 
 
-# read command-line parameters
-parser = argparse.ArgumentParser('Evaluating the BERT / ALBERT NLU model')
-parser.add_argument('--model', '-m', help = 'Path to BERT / ALBERT NLU model', type = str, required = True)
-parser.add_argument('--type', '-tp', help = 'bert or albert', type = str, default = 'bert', required = False)
+if __name__ == "__main__":
+    # Reads command-line parameters
+    parser = argparse.ArgumentParser("Evaluating the BERT NLU model")
+    parser.add_argument("--model", "-m",
+                        help="Path to BERT NLU model",
+                        type=str,
+                        required=True)
+    
+    args = parser.parse_args()
+    load_folder_path = args.model
+    
 
+    # this line is to disable gpu
+    os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
-VALID_TYPES = ['bert', 'albert']
+    config = tf.ConfigProto(intra_op_parallelism_threads=1,
+                            inter_op_parallelism_threads=1,
+                            allow_soft_placement=True,
+                            device_count = {"CPU": 1})
+    sess = tf.compat.v1.Session(config=config)
 
-args = parser.parse_args()
-load_folder_path = args.model
-type_ = args.type
+    bert_model_hub_path = "/content/drive/MyDrive/ChatBot/codes/bert-module"
+    
+    vocab_file = os.path.join(bert_model_hub_path, 'assets/vocab.korean.rawtext.list')
 
-# this line is to disable gpu
-os.environ["CUDA_VISIBLE_DEVICES"]="-1"
+    bert_to_array = BERTToArray(vocab_file)
 
-config = tf.ConfigProto(intra_op_parallelism_threads=1,
-                        inter_op_parallelism_threads=1,
-                        allow_soft_placement=True,
-                        device_count = {'CPU': 1})
-sess = tf.compat.v1.Session(config=config)
+    print('Loading Models...')
+    if not os.path.exists(load_folder_path):
+        print(f'Folder {load_folder_path} not exist')
 
-if type_ == 'bert':
-############################### TODO 경로 고치기 ##########################################
-    bert_model_hub_path = '/content/drive/MyDrive/bert-module'
-###########################################################################################
-    is_bert = True
-elif type_ == 'albert':
-    bert_model_hub_path = 'https://tfhub.dev/google/albert_base/1'
-    is_bert = False
-else:
-    raise ValueError('type must be one of these values: %s' % str(VALID_TYPES))
+    tags_to_array_path = os.path.join(load_folder_path, 'tags_to_array.pkl')
+    with open(tags_to_array_path, 'rb') as handle:
+        tags_to_array = pickle.load(handle)
+        slots_num = len(tags_to_array.label_encoder.classes_)
 
+    model = BertSlotModel.load(load_folder_path, sess)
 
-############################### TODO ##########################################
-# 모델과 벡터라이저 불러오기
-###############################################################################
+    while True:
+        print('Enter Your Sentence : ')
 
+        try :
+            input_text = input().strip()
 
-while True:
-    print('\nEnter your sentence: ')
-    try:
-        input_text = input().strip()
-    except:
-        continue
-        
-    if input_text == 'quit':
-        break
+        except :
+            continue
 
-############################### TODO ##########################################
-# 사용자가 입력한 한 문장을 슬롯태깅 모델에 넣어서 결과 뽑아내기
-###############################################################################
+        if input_text in ['끝', '꺼']:
+            break
 
-tf.compat.v1.reset_default_graph()
+        else :
+            text_arr = bert_to_array.tokenizer.tokenize(input_text)
 
+            input_ids, input_mask, segment_ids = bert_to_array.transform([' '.join(text_arr)])
+
+            inferred_tags, slot_score = model.predict_slots([input_ids, input_mask, segment_ids], tags_to_array)
+
+            print(text_arr)
+            print(inferred_tags[0])
+            print(slot_score[0])
+    
+    tf.compat.v1.reset_default_graph()
+    
